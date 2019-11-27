21baa2be53eeff2cfbfdb1e3beb4391438012739
iris <52894745+sudocode1@users.noreply.github.com>
2019-11-27 15:03:51 +0000

    fixed some stuff and new commands

--- commands/addrole.js
+++ commands/addrole.js
@@ -5,15 +5,14 @@ module.exports.run = async (bot, message, args) => {
 if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("yea no");
 let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 if(!rMember) return message.reply("No user specified.");
-let role = args.join(" ").slice(22);
+let role = message.guild.roles.find(r => r.name.toLowerCase() === args.slice(1).join(" ").toLowerCase())
 if(!role) return message.role("No role specified.");
-let gRole = message.guild.roles.find(`name`, role);
-if(!gRole) return message.reply("Role not found. Role names are cAsE sEnSiTiVe.");
+if(message.member.highestRole.position <= role.position) return message.channel.send('You aren\'t high enough in the role hierchy!');
+    
+if(rMember.roles.has(role.id)) return message.reply("They already have that role.");
+await(rMember.addRole(role.id));
 
-if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
-await(rMember.addRole(gRole.id));
-
-message.channel.send(`Given the role ${gRole.name} to <@${rMember.id}>`)
+message.channel.send(`Given the role ${role.name} to <@${rMember.id}>`)
 
 }
 
--- commands/removerole.js
+++ commands/removerole.js
@@ -5,15 +5,14 @@ module.exports.run = async (bot, message, args) => {
 if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("yea no");
 let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 if(!rMember) return message.reply("No user specified.");
-let role = args.join(" ").slice(22);
+let role = message.guild.roles.find(r => r.name.toLowerCase() === args.slice(1).join(" ").toLowerCase())
 if(!role) return message.role("No role specified.");
-let gRole = message.guild.roles.find(`name`, role);
-if(!gRole) return message.reply("Role not found. Role names are cAsE sEnSiTiVe.");
+if(message.member.highestRole.position <= role.position) return message.channel.send('You aren\'t high enough in the role hierchy!');
+    
+if(rMember.roles.has(role.id)) return message.reply("They don\'t have that role.");
+await(rMember.removeRole(role.id));
 
-if(!rMember.roles.has(gRole.id)) return message.reply("They don't have that role.");
-await(rMember.removeRole(gRole.id));
-
-message.channel.send(`Removed the role ${gRole.name} from <@${rMember.id}>`)
+message.channel.send(`Removed the role ${role.name} from <@${rMember.id}>`)
 
 }
 
